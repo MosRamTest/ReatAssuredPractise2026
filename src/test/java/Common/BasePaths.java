ad8e1aac40a4d4e4c3997f3617547d74f93b4855
MosRamTest <mosram2@gmail.com>
2026-05-27 19:07:08 +0200

    Added Request Builder

--- src/test/java/Common/BasePaths.java
+++ src/test/java/Common/BasePaths.java
@@ -3,4 +3,5 @@ package Common;
 public class BasePaths {
 
   public static String baseUrl = "https://openweathermap.org/api";
+  public static String baseUrl2 = "https://ndosiautomation.co.za/";
 }
--- src/test/java/RequestBuilder/APIRequestBuilder.java
+++ src/test/java/RequestBuilder/APIRequestBuilder.java
@@ -1,4 +1,23 @@
 package RequestBuilder;
 
-public class test {
+import io.restassured.response.Response;
+
+import static Common.BasePaths.*;
+import static io.restassured.RestAssured.given;
+import static PayloadBuilder.APIPayloadBuilder.*;
+
+public class APIRequestBuilder {
+
+    public static Response loginResponse(){
+        return.given().
+                baseUri(baseUrl2).
+                basePath("/login").
+                contentType("application/json").
+                body(loginPayload()).
+                log().all().
+                post().
+                then().
+                log().all().
+                extract().response();
+    }
 }
